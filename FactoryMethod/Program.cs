952f194bfbc45f87e65631723ab4d73badf20b8c
volkanp <vpaksoy@rcpsych.ac.uk>
2015-10-14 18:41:45 +0100

    Added comments

--- FactoryMethod/Program.cs
+++ FactoryMethod/Program.cs
@@ -12,9 +12,18 @@ namespace FactoryMethod
         static void Main(string[] args)
         {
             var feedSettings = new AppConfigFeedSettings();
+            // This instance is created by the abstract factory in the Rareburg.ArticleFeedGenerator project
+
             var feed = new SyndicationFeed();
+            // Just a blank feed for demonstration
+
             var feedFormatterFactory = FeedFormatterFactory.CreateFactory(feed, feedSettings);
+            // This static method is an example of what GoF defines as "Parameterized factory methods":
+            // "The factory method takes a parameter that identifies the kind of object to create"
+
             var feedFormatter = feedFormatterFactory.CreateFeedFormatter();
+
+
         }
     }
 }
