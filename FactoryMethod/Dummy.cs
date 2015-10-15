e9d459f6186d92b1ae69624d0b9acc215da788cf
volkanp <vpaksoy@rcpsych.ac.uk>
2015-10-15 15:10:11 +0100

    Added "Product" classes to the diagram

---
+++ FactoryMethod/Dummy.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FactoryMethod
+{
+    /*
+    public class SyndicationFeedFormatter
+    {
+    }
+
+    public class Atom10FeedFormatter : SyndicationFeedFormatter
+    {
+    }
+
+    public class Rss20FeedFormatter : SyndicationFeedFormatter
+    {
+    }
+    */
+}
